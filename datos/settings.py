d4c688918711421d64635178ba905bad5286b2cf
Josué <126045947+JosueEUsecheCh@users.noreply.github.com>
2025-02-07 14:05:58 -0400

    Actualizar ALLOWED_HOSTS para permitir dominio de Render

--- datos/settings.py
+++ datos/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Asegúrate de agregarlo antes de 'django.middleware.security.SecurityMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'datos.urls'
 
@@ -133,6 +135,9 @@ STATIC_URL = '/static/'
 
 # En un entorno de producción, asegúrate de que STATIC_ROOT esté configurado
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # Default primary key field type
