754bf6f1be1756a930717e7b25a14e0a36273fd0
Josué <126045947+JosueEUsecheCh@users.noreply.github.com>
2025-02-08 00:58:42 -0400

    Actualizar .env

--- datos/settings.py
+++ datos/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-53ke!c-=_42wp2@60jjpdzktqe%d^zg8zz)brsq-te#6)u$g$p'
+
+load_dotenv()  # Cargar las variables del archivo .env
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
