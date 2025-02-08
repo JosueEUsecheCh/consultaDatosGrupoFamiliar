5cc23e527d64cf5b0f2777db761105d2ac2ba1e7
Josué <126045947+JosueEUsecheCh@users.noreply.github.com>
2025-02-08 01:49:35 -0400

    update ancrypt

--- principal/models.py
+++ principal/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from cryptography.fernet import Fernet
+import os
+from dotenv import load_dotenv
+
+# Cargar variables desde el archivo .env
+load_dotenv()  # Esto carga el archivo .env
+KEY = os.getenv("ENCRIPTYON_KEY")  # Cargar la clave de encriptación desde el archivo .env
+
+# Crear una instancia de Fernet con la clave
+cipher_suite = Fernet(KEY)
 
 class MyTable(models.Model):
     ci_titular = models.CharField(max_length=20, primary_key=True)
@@ -17,5 +27,23 @@ class MyTable(models.Model):
         managed = False  # Django no gestionará esta tabla
         db_table = 'mytable'
 
+    def encrypt_ci(self, ci):
+        """Encripta la cédula"""
+        return cipher_suite.encrypt(ci.encode()).decode()
+
+    def decrypt_ci(self, encrypted_ci):
+        """Desencripta la cédula"""
+        return cipher_suite.decrypt(encrypted_ci.encode()).decode()
+
+    def save(self, *args, **kwargs):
+        # Encriptamos las cédulas antes de guardarlas
+        if self.ci_titular:
+            self.ci_titular = self.encrypt_ci(self.ci_titular)
+        if self.ci_beneficiario:
+            self.ci_beneficiario = self.encrypt_ci(self.ci_beneficiario)
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return f"{self.nombres} {self.apellidos} ({self.ci_titular})"
+        # Desencriptamos la cédula para mostrarla
+        ci_titular = self.decrypt_ci(self.ci_titular) if self.ci_titular else "Desconocido"
+        return f"{self.nombres} {self.apellidos} ({ci_titular})"
